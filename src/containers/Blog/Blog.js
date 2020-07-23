858e66f85e1a301bc574e9027acd3064d2ebc7a3
florianbar <florianbar01@gmail.com>
2020-07-23 13:52:36 +0200

    created custom axios instance

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
+import axiosInstance from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -14,7 +15,7 @@ class Blog extends Component {
     };
 
     componentDidMount () {
-        axios.get("/posts")
+        axiosInstance.get("/posts")
             .then(response => {
                 const posts = response.data.slice(0, 4); //only get 4 posts
                 const updatedPosts = posts.map(post => {
