d43c67bcf9175780c66f0f89e4fe73ccb885186c
florianbar <florianbar01@gmail.com>
2020-07-23 13:28:51 +0200

    Setting up axios defaults and interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+//Use interceptors to execute REQUEST code globally
+axios.interceptors.request.use(request => {
+    console.log(request);
+    return request; //always return the request, otherwise you are blocking it
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+//Use interceptors to execute RESPONSE code globally
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return response; //always return the request, otherwise you are blocking it
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
