465f360f68f6a79146ef5fd0c0ec9079f4ef6352
florianbar <florianbar01@gmail.com>
2020-08-04 10:35:13 +0200

    Added Link to navigate to Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -11,13 +12,18 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: "/new-post",
+                                hash: "#submit",
+                                search: "?quick-submit=true"
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route exact path="/" render={() => <h1>Home</h1>} /> */}
                 <Route exact path="/" component={Posts} />
+                <Route exact path="/new-post" component={NewPost} />
             </div>
         );
     }
